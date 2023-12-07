0bebadf0ed2b8351dc31a14de62244fa5ec5d570
HowForRetainingYou <3058602461@qq.com>
2023-12-07 20:28:53 +0800

    .

---
+++ 2023.12.5/2023.12.5/2023.12.7.c
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
+int main()
+//{
+//	{
+//		int a, b, c;
+//		a = 5;
+//		c = ++a;
+//		b = ++c, c++, ++a, a++;//只赋值++c给b，其余的c++ 与++a a++；
+//		b += a++ + c;//b=b+(a++ + c)
+//		printf("a = %d b = %d c = %d\n:", a, b, c);
+//		return 0;
+//	}
+//}//a+=x等效于a=a+x
+
+
+	/*int a,b;
+
+	scanf("%d %d", &a, &b);
+	printf("%d,%d", a, b);
+	return 0;*/
+
+
+
+
+//
+//	int i;//死循环
+//	for (i = 0; i <= 10; ++i)
+//		if(i=5)
+//		printf("%d", i);
+//	return 0;
+//}//同时按下Ctrl+C可终止程序 
+
+
+
+
+	//{
+	//	int ret = strcmp("abc", "cba");
+	//	printf("%d\n", ret);
+	//	return 0;
+	//	//比较规则：一个一个去比较，根据首次出现的不一样的字符进行比较（内有ASCII值）
+	//	//输出结果小于零代表第一个字符串小于第二个，等于零则代表等于，大于零则代表大于
+	//}
